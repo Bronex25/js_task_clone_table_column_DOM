59c78844958dbfb17513dfa93991bda5e4402a03
unknown <oleksandr.plaksin25@gmail.com>
2024-08-10 08:42:16 +0200

    make build

---
+++ index.d98dff3a.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,t=0;t<e.length;t++){var n=document.createElement(0===t||t===e.length-1?"th":"td");n.innerText=e[t].cells[1].innerText,e[t].insertBefore(n,e[t].lastElementChild)}
+//# sourceMappingURL=index.d98dff3a.js.map
